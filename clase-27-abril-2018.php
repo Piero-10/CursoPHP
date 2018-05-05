8a55b72da18eba1edbf457312688b2661a0eaf55
Piero-10 <38970746+Piero-10@users.noreply.github.com>
2018-05-04 19:30:37 -0500

    Archivo de clase
    ultima clase del curso de php desde cero

---
+++ clase-27-abril-2018.php
@@ -0,0 +1,30 @@
+<?php
+final class UnaClase{
+  public function mimetodo(){
+    
+  }
+}
+
+class Hija extends UnaClase{
+  
+}
+
+$object = new Hija();
+/*
+interface Claseinter {  
+  public function mimetodo();
+  public function mimetodo2(); 
+}
+
+class OperationsArithmetic implements Claseinter{
+  public $nameObject="";
+  public function mimetodo(){
+    
+  }
+  public function mimetodo2(){
+    
+  }
+}
+
+$object1 = new OperationsArithmetic("1");
+*/
